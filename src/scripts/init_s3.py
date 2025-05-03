a5d20eed7b033065417d2225d3a016509d23d248
Pestov-roman <Pestov.Roman.N@yandex.ru>
2025-05-03 09:53:18 +0500

    fix bags

--- src/scripts/init_s3.py
+++ src/scripts/init_s3.py
@@ -3,7 +3,11 @@ from src.utils import s3 as _s3
 
 
 async def ensure_bucket() -> None:
-    client = _s3._get_client()
+    # В CI/CD используем localhost вместо minio
+    endpoint = (
+        "http://localhost:9000" if settings.app_env == "ci" else settings.s3_endpoint
+    )
+    client = _s3._get_client(endpoint_url=endpoint)
     buckets = {b["Name"] for b in client.list_buckets()["Buckets"]}
     if settings.s3_bucket not in buckets:
         client.create_bucket(Bucket=settings.s3_bucket)
