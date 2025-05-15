d7c031915fbe245d3fe1b3cb7f5ff1f416cd06f9
Pestov-roman <Pestov.Roman.N@yandex.ru>
2025-05-15 12:32:37 +0500

    fix bags

--- src/tasks/email.py
+++ src/tasks/email.py
@@ -1,10 +1,9 @@
-
 from celery import shared_task
 
 from src.utils.email import send_email
 
 
-@shared_task(
+@shared_task(  # type: ignore
     name="email.send_registration",
     autoretry_for=(Exception,),
     retry_kwargs={'max_retries': 3, 'countdown': 2}
