2257915f3531dc4d2004155fe24a0ecc6a08a193
Pestov-roman <Pestov.Roman.N@yandex.ru>
2025-05-15 12:23:41 +0500

    fix bags

--- src/tasks/email.py
+++ src/tasks/email.py
@@ -1,3 +1,4 @@
+
 from celery import shared_task
 
 from src.utils.email import send_email
@@ -6,7 +7,7 @@ from src.utils.email import send_email
 @shared_task(
     name="email.send_registration",
     autoretry_for=(Exception,),
-    retry_kwargs={'max_retries': 3, 'countdown': 2},
+    retry_kwargs={'max_retries': 3, 'countdown': 2}
 )
 def send_registration_email(to: str) -> None:
     import asyncio
